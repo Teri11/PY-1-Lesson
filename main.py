1498724856cffb9a232e32c64bc84a4f7c6071d5
Teri11 <augustineteri157@gmail.com>
2022-11-01 16:18:08 +0300

    added a menu robot reply

---
+++ main.py
@@ -0,0 +1,13 @@
+#Lets build a robot Barista.......
+print("Hello, Welcome  to Sharp Jr. Coffee sessions!!!!!!!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello" +" " + name + "," " thank you so much for coming in today\n")  
+menu = "Expresso, Black Coffee, Latte, Cappuciano"
+
+print(name +", " + "What would you like from our menu today? Here is what we are serving.\n" + menu)
+
+order = input()
+
+print("Sounds good"+" "+ name+ ", we will have that"+" " +order+" "+"ready for you in a moment.")
